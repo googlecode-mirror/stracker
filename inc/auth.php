6e8c5fd6ed0296089b626f230ac7582ecb08c88b
Time Sheep <zipnokaj@gmail.com>
2012-06-03 03:16:15 +0200

    Finished auth_register and assigned result of g_defError() to a variable.

--- inc/auth.php
+++ inc/auth.php
@@ -12,27 +12,25 @@
    g_req(array("general"));               //
 ////////////////////////////////////////////
 
-function _auth_validate($takeuser,$takepass){
+function auth_validate($takeuser,$takepass){
 	// Check Captcha
 	if ($_POST["recaptcha_response_field"]) { // Check wether the user entered something in the field
         $resp = recaptcha_check_answer ($cfg['privatekey'],$_SERVER["REMOTE_ADDR"], $_POST["recaptcha_challenge_field"], $_POST["recaptcha_response_field"]); // Check the response
 		if (!$resp->is_valid) { // The captcha is invalid
-			g_defError(true, 1, $lang['wrongcaptcha'],$resp->error);
+			$e = g_defError(true, 1, $lang['wrongcaptcha'],$resp->error);
         }
 	}else{
 		g_defError(true, 0, $lang['nocaptcha']); // Return error code 0 - Captcha field was empty
 	}
 	// Verify username/password combination
 	if($err['error'] === false)
-		$db_handle = mysql_connect($cfg['db_host'],$cfg['db_user'],$cfg['db_host']) or die(mysql_error()); // Connect to MySQL server or die and print error
-		mysql_select_db($cfg['db_host']) or die(mysql_error()); // Select the database containing the STracker info
-		$db_result = mysql_query("SELECT uid,salt,password FROM users WHERE username='" . $takeuser . "'",$db_handle); // Query the database for the user information
-		$db_result = mysql_fetch_array($db_result)
+		$db_result = mysql_query("SELECT uid,salt,password FROM users WHERE username='" . $takeuser . "'"); // Query the database for the user information
+		$db_result = mysql_fetch_array($db_result);
 		if($db_result['uid']){ // If a unique user ID is returned by the database
-			g_defError(false, 200, $lang['correctuser']); // Return the OK error code
+			$e = g_defError(false, 200, $lang['correctuser']); // Return the OK error code
 			$realpass = hash("sha1",hash("sha512",hash("whirlpool",$takepass . $db_result['salt']))); // Encrypt the password and salt using SHA1->SHA512->Whirlpool encryption
 			if($realpass == $db_result['password']){ // Compare hashed and salted passwords from the form and the DB
-				g_defError(false, 200, $lang['correctuser']); // Return the OK error code
+				$e = g_defError(false, 200, $lang['correctuser']); // Return the OK error code
 				$_SESSION['uid'] = $db_result['uid']; // Set the session variable so we can check which user is logged in
 				if($_SESSION['prevpage']){ // Check wether the user is trying to access a restricted page before sent to login
 					$redirect_loc = $_SESSION['prevpage']; // Redirect to previous page
@@ -43,8 +41,37 @@ function _auth_validate($takeuser,$takepass){
 				header("Location: " . $redirect_loc); // Set the location header
 			}
 		}else{
-			g_defError(true, 2, $lang['wronguser']); // Return error code 2 - username does not exist
+			$e = g_defError(true, 2, $lang['wronguser']); // Return error code 2 - username does not exist
 		}
 	}
 	return $err;
+}
+
+
+
+
+function auth_register($takeuser,$takepass,$takepass2,$takemail,$takemail2){
+	$mail_tpl = mysql_fetch_array(mysql_query("SELECT * FROM mails WHERE active=true AND type='registration'"));
+	$resp = recaptcha_check_answer ($cfg['privatekey'],$_SERVER["REMOTE_ADDR"], $_POST["recaptcha_challenge_field"], $_POST["recaptcha_response_field"]); // Check captcha response
+	if(!$takeuser | !$takepass | !$takepass2 | !$takemail | !$takemail2){
+		$e = g_defError(true,3,$lang['fieldmissing']);
+	}elseif($takepass != $takepass2){
+		$e = g_defError(true,4,$lang['passmismatch']);
+	}elseif($takemail != $takemail2){
+		$e = g_defError(true,4,$lang['mailmismatch']);
+	}elseif(mysql_num_rows(mysql_query("SELECT username LIKE $takeuser"))){
+		$e = g_defError(true,6,$lang['usernameexists']);
+	}elseif(!$resp->is_valid) {
+		$e = g_defError(true, 1, $lang['wrongcaptcha'],$resp->error);
+    }else{
+		$salt = md5(substr(str_shuffle(str_repeat('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789',5)),0,5));
+		$encryptedpass = hash("sha1",hash("sha512",hash("whirlpool",$takepass . $salt)));
+		mysql_query("INSERT INTO users SET username=$takeuser,password=$encryptedpass,salt=$salt,email=$takemail,active=no");
+		$mail_tpl = mysql_fetch_array(mysql_query("SELECT * FROM mails WHERE active=true AND type='registration'"));
+		$subject = str_replace("%username%",$takeuser,$mail_tpl['subject']);
+		$message = str_replace("%username%",$takeuser,$mail_tpl['message']);
+		$message = str_replace("%link%",$cfg['baseurl'] . "activate.php?salt=" . $salt,$message);
+		mail($takemail,$subject,$message,$mail_tpl['header']);
+		$e = g_defError(true, 200, $lang['reg_complete']); // OK code - registered correctly
+	}
 }
